8ba90f02b1cdaf319a065bca7ca27bd2ebf1d90b
vknec <vkcse@nec.edu.in>
2016-09-25 19:54:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article one', function (req, res) {
+  res.sendFile( 'article one is requested and will be served here');
+});
+
+app.get('/article two', function (req, res) {
+  res.sendFile( 'article two is requested and will be served here');
+});
+
+app.get('/article three', function (req, res) {
+  res.sendFile( 'article three is requested and will be served here');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
